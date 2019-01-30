3efeac199d9e07de01247d08c1c75839ab03ba82
Darryl <darrylmangibin@gmail.com>
2019-01-30 19:08:44 +0800

    UPDATE - server.js

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const {User} = require('./models/User')
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json())
 
 app.post('/todos', (req, res) => {
@@ -45,8 +47,8 @@ app.get('/todos/:id', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log(`Started on port 3000`)
+app.listen(port, () => {
+    console.log(`Started on port ${port}`)
 })
 
 module.exports = { app }
